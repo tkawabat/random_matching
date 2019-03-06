e554be20f9571442c99cae07715a4c2a40548091
tkawabat <tosiki.kawabata@gmail.com>
2019-03-06 17:31:59 +0900

    fix

--- src/twitter.js
+++ src/twitter.js
@@ -1,8 +1,16 @@
-'use strict';
+"use strict";
 
 const twitter = require("twitter");
 const fs = require("fs");
 
 const client = new twitter(JSON.parse(fs.readFileSync("secret.json","utf-8")));
+
+client.get("account/settings", {}, function(error, tweet, response){
+    if (!error) {
+        console.log(tweet);
+    } else {
+        console.log(error);
+    }
+});
 
 exports.twitter = twitter;
