2f45444052356041784a921ef7f0d1ebf80b9805
tkawabat <tosiki.kawabata@gmail.com>
2019-03-09 18:00:05 +0900

    socket validation

--- routes/user.js
+++ routes/user.js
@@ -7,8 +7,7 @@ const router = express.Router();
 const account = require(rootDir + "/src/account");
 
 
-router.get("/", function(req, res, next) {
-    console.log(req.user);
+router.get("/", account.isAuthenticated, function(req, res) {
     res.render("user", { title: "らんだむまっちんぐ", user: req.user });
 });
 
--- src/socket.js
+++ src/socket.js
@@ -11,12 +11,42 @@ const set = function(server) {
         .use(account.socketSession)
     ;
 
+
     io.on("connection", (socket) => {
-        socket.on("save skype", function(msg) {
-            console.log(msg);
-        
+        // validation
+        socket.use((packet, next) => {
+            let user = socket.request.user;
+            let msg = packet[1];
+            if (msg.sex != "f") {
+                sendError("性別が正しくありません");
+                return;
+            }
+            if (msg.skype_id.length < 3) {
+                sendError("skype idが正しくありません");
+                return;
+            }
+
+            return next();
+
+
+            //next(new Error('validation error'));
         });
+
+        const sendError = function(msg) {
+            let user = socket.request.user;
+            console.log("error send:"+user.name);
+            socket.emit("error", {error: msg});
+        };
+        const userSave = function(msg) {
+            let user = socket.request.user;
+            console.log("user save:"+user.name);
+
+            sendError("なんかえらー");
+        };
+
+        socket.on("user save", userSave);
     });
+
 }
 
 
