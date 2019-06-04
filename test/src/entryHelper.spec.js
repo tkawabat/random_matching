65d31adf30870287f7691e1d69071d715a056484
tkawabat <tosiki.kawabata@gmail.com>
2019-06-04 14:11:02 +0000

    add test

--- test/src/entryHelper.spec.js
+++ test/src/entryHelper.spec.js
@@ -1,19 +1,23 @@
 "use strict";
 
 const rootDir = require("app-root-path");
+const moment = require("moment-timezone");
+moment.tz.setDefault("Asia/Tokyo");
 const expect = require("expect");
 const sinon = require("sinon");
 
 const schedule = require(rootDir+"/src/schedule");
 const twitter = require(rootDir+"/src/twitter");
 const entryHelper = require(rootDir+"/src/entryHelper");
+const matcher = require(rootDir+"/src/matcher");
 const Match = require(rootDir+"/src/model/match");
 const Entry = require(rootDir+"/src/model/entry");
 
 
-let userId = "100";
+let stubs = [];
 
 describe("entryHelper get", () => {
+    let userId = "100";
     let req, res;
     beforeEach(async () => {
         req = {
@@ -78,4 +82,49 @@ describe("entryHelper get", () => {
         }
         expect(actual).toEqual(entry);
     });
+});
+
+describe("entryHelper pushScheduleMatch", () => {
+    let mock;
+
+    before(() => {
+        stubs.push(sinon.stub(matcher, "match"));
+        stubs.push(sinon.stub(twitter, "tweet"));
+    });
+    beforeEach(() => {
+        mock = sinon.mock(schedule);
+    });
+    afterEach(() => {
+        mock.restore();
+    });
+    after(() => {
+        for (let s of stubs) s.restore();
+    });
+
+    it("ng type invalid", async () => {
+        let entry = { type: ["act2"] };
+        mock.expects("push").never();
+        entryHelper.pushScheduleMatch(entry);
+        mock.verify();
+    });
+
+    it("ng already scheduled", async () => {
+        let entry = { type: ["act3_7"] };
+        let time = moment().add(30, "minutes");
+        schedule.push("match_2_act3_7", true, time.toDate(), () => {});
+
+        mock.expects("push").never();
+        entryHelper.pushScheduleMatch(entry);
+        mock.verify();
+
+        schedule.cancel("match_2_act3_7");
+    });
+
+    it("ok", async () => {
+        let entry = { type: ["act3_7"] };
+        mock.expects("push").twice();
+        entryHelper.pushScheduleMatch(entry);
+        mock.verify();
+    });
+
 });
